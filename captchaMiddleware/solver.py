60f21c1f50506efba9bc832f85bbf0ceed1fd30a
Owen Miller <owen9825@gmail.com>
2018-12-09 15:55:38 -0500

    Updating compatibility for Tesseract v4; collateral PEP8 changes

--- captchaMiddleware/solver.py
+++ captchaMiddleware/solver.py
@@ -83,40 +83,54 @@ def solveCaptcha(imgUrl, brazen=False):
 
 
 def applyOcr(imgUrl):
-    response = urllib.urlopen(imgUrl);
-    img = np.asarray(bytearray(response.read()), dtype="uint8");
-    src = cv2.imdecode(img, cv2.IMREAD_GRAYSCALE);
+    """
+    Open an image and read its letters.
+    :param imgUrl: The URL for a CAPTCHA image.
+    :return: The string in the imgae.
+    """
+    response = urllib.urlopen(imgUrl)
+    img = np.asarray(bytearray(response.read()), dtype="uint8")
+    gray_img = cv2.imdecode(img, cv2.IMREAD_GRAYSCALE)
     # if it's black on white:
-    src = 255 - src;
-    _, mask = cv2.threshold(src, THRESHOLD, 255, cv2.THRESH_BINARY);
-    mask, contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE);
-    coloured = cv2.cvtColor(src, cv2.COLOR_GRAY2RGB);
-    letters = {};
+    gray_img = 255 - gray_img
+    _, mask = cv2.threshold(gray_img, THRESHOLD, 255, cv2.THRESH_BINARY)
+    mask, contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    letters = {}  # x coordinate ↦ predicted letter
     for c, contour in enumerate(contours):
-        rect = cv2.minAreaRect(contour); # this is used for the angle
+        rect = cv2.minAreaRect(contour)  # Find a rotated bounding box
         # rect is a tuple: ((corner 1, corner2), angle)
-        angle = adjustAngle(rect[-1]);
-        letterMask = np.zeros(mask.shape, dtype="uint8");
-        letterMask = cv2.drawContours(letterMask, [contour], contourIdx=0, color=255, thickness=-1);
-        imgROI = cv2.bitwise_and(src, src, mask=letterMask);        
-        coloured = cv2.cvtColor(imgROI, cv2.COLOR_GRAY2RGB);
-        rotated = imutils.rotate_bound(coloured, -angle);
-        rotated = 255 - rotated;
-        pilImg = Image.fromarray(rotated);
-        charResult = image_to_string(pilImg, config="-psm 10");
-        if charResult is not None and len(charResult) > 0:
-            moments = cv2.moments(contour);
-            xCentre = int(moments["m10"]/moments["m00"]);
+        angle = adjustAngle(rect[-1])
+        # Draw this contour
+        letter_mask = np.zeros(mask.shape, dtype="uint8")
+        letter_mask = cv2.drawContours(letter_mask, [contour], contourIdx=0, color=255,
+                                       thickness=-1)
+        segmented_img = cv2.bitwise_and(gray_img, gray_img, mask=letter_mask)  # Applying mask
+        dark_spun = imutils.rotate_bound(segmented_img, -angle)
+        """
+        We could crop the letter, but Tesseract doesn't seem to like such tight margins.
+        brightPixels = cv2.findNonZero(dark_spun)
+        x, y, width, height = cv2.boundingRect(brightPixels)
+        cropped = darkSpun[y:y+height, x:x+width]
+        """
+        colored = cv2.cvtColor(dark_spun, cv2.COLOR_GRAY2RGB)
+        colored = 255 - colored
+        pil_image = Image.fromarray(colored)
+        char_result = image_to_string(pil_image, config="--psm 10 --oem 0")
+        if char_result is not None and len(char_result) > 0:
+            moments = cv2.moments(contour)
+            xCentre = int(moments["m10"]/moments["m00"])
             while xCentre in letters:
-                xCentre += 1; # Avoid key clash
-            letters[xCentre] = charResult.upper();
+                xCentre += 1  # Avoid key clash
+            letters[xCentre] = char_result.upper()
             if logger.getEffectiveLevel() <= logging.DEBUG:
-                scipy.misc.imsave(charResult + ".jpg", pilImg);
+                scipy.misc.imsave(char_result + ".jpg", pil_image)
         else:
-            logger.debug("No result for character %d", c);
+            logger.debug("No result for character %d", c)
+            if logger.getEffectiveLevel() <= logging.DEBUG:
+                scipy.misc.imsave("unknown letter {c}.jpg".format(c=c), pil_image)
     # Adjust letters based on X axis
-    wordSolution = "";
+    word_solution = ""
     for xCentre in sorted(letters.keys()):
-        wordSolution += letters[xCentre];
-    logger.debug("OCR saw %s", wordSolution);
-    return wordSolution;
+        word_solution += letters[xCentre]
+    logger.debug("OCR saw %s", word_solution)
+    return word_solution
